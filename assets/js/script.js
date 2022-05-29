c1b63113d6a13a975db87a13feb7afe1360c5cbe
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-05-29 03:19:29 +0100

    func to get forecast data from api added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,68 @@ const weatherCardsContainer = $("#weather-cards-container");
 // Open weather API
 const API_KEY = "60687e3c50614663696f05b189c2493e";
 
-// Get current data from API
-const getCurrentData = function (name, forecastData) {
+// collect data from API
+const collectCurrentData = function (name, forecastData) {
   return {
     name: name,
     temperature: forecastData.current.temp,
     wind: forecastData.current.wind_speed,
     humidity: forecastData.current.humidity,
     uvi: forecastData.current.uvi,
-    date: getFormattedDate(forecastData.current.dt, "ddd DD/MM/YYYY HH:mm"),
+    date: collectFormattedDate(forecastData.current.dt, "ddd DD/MM/YYYY HH:mm"),
     iconCode: forecastData.current.weather[0].icon,
   };
 };
 
-const getFormattedDate = function (unixTimestamp, format = "DD/MM/YYYY") {
+const collectFormattedDate = function (unixTimestamp, format = "DD/MM/YYYY") {
   return moment.unix(unixTimestamp).format(format);
+};
+// Get forecast data from API
+const collectForecastData = function (forecastData) {
+  const callback = function (each) {
+    return {
+      date: collectFormattedDate(each.dt),
+      temperature: each.temp.max,
+      wind: each.wind_speed,
+      humidity: each.humidity,
+      iconCode: each.weather[0].icon,
+    };
+  };
+
+  return forecastData.daily.slice(1, 6).map(callback);
+};
+
+const collectWeatherData = async (cityName) => {
+  const currentDataUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+  const currentDataResponse = await fetch(currentDataUrl);
+  const currentData = await currentDataResponse.json();
+
+  const lat = currentData.coord.lat;
+  const lon = currentData.coord.lon;
+  const name = currentData.name;
+
+  const forecastDataUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`;
+
+  const forecastDataResponse = await fetch(forecastDataUrl);
+  const forecastData = await forecastDataResponse.json();
+
+  const current = collectCurrentData(name, forecastData);
+  const forecast = collectForecastData(forecastData);
+
+  return {
+    current: current,
+    forecast: forecast,
+  };
+};
+
+const collectUVIClassName = function (uvi) {
+  if (uvi >= 0 && uvi < 3) {
+    return "btn-success";
+  } else if (uvi >= 3 && uvi < 6) {
+    return "btn-warning";
+  } else if (uvi >= 6 && uvi < 8) {
+    return "btn-danger";
+  } else {
+    return "btn-dark";
+  }
 };
